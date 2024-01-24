6211ef2fa81b60a9cbcb36de98479f40e26a1afb
Augusto <gutodiasdev@gmail.com>
2024-01-24 17:08:40 -0300

    feat: add SaveUserRepository

--- src/data/contracts/user.ts
+++ src/data/contracts/user.ts
@@ -1,3 +1,22 @@
 export interface IsEmailAlreadyRegisteredRepository {
-  isEmailAlreadyRegistered(email: string): Promise<boolean>
+  isEmailAlreadyRegistered(email: IsEmailAlreadyRegisteredRepository.Input): Promise<IsEmailAlreadyRegisteredRepository.Output>
+};
+
+export namespace IsEmailAlreadyRegisteredRepository {
+  export type Input = string
+  export type Output = boolean
+};
+
+export interface SaveUserRepository {
+  save(input: SaveUserRepository.Input): Promise<SaveUserRepository.Output>
+}
+
+export namespace SaveUserRepository {
+  export type Input = {
+    id: string;
+    name: string;
+    email: string;
+    phone: string;
+  }
+  export type Output = void
 }
