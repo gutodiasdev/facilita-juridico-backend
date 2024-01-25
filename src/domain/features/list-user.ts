ede8c02c206dfc438fb069ece79641576e0bc0ca
Augusto <gutodiasdev@gmail.com>
2024-01-24 22:02:38 -0300

    feat: add ListUsers interface

---
+++ src/domain/features/list-user.ts
@@ -0,0 +1,12 @@
+import { User } from "../@types"
+
+export interface ListUsers {
+  execute(input: ListUsers.Input): Promise<ListUsers.Output>
+}
+
+export namespace ListUsers {
+  export type Input = {
+    term?: string;
+  }
+  export type Output = User[]
+}
