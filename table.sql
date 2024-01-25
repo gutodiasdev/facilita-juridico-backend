9a368cae0e52159a8ae42bbca60f8a48abb15a5a
Augusto <gutodiasdev@gmail.com>
2024-01-25 14:02:31 -0300

    chore: add user insertion at table.sql

--- table.sql
+++ table.sql
@@ -1,6 +1,8 @@
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
+    id VARCHAR(255) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     phone VARCHAR(20) NOT NULL
 );
+
+INSERT INTO users (id, name, email, phone) VALUES ('test-id', 'Test User', 'test@example.com', '5591999123456');
